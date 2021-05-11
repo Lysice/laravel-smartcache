15f5ce68d1b60ae6aaf4b052007d580ed1497f91
lysice <snowist@126.com>
2021-05-11 11:52:27 +0800

    method

--- src/SecondaryCache.php
+++ src/SecondaryCache.php
@@ -4,7 +4,15 @@ namespace Lysice\Cache;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class SecondaryCache
+ * @package Lysice\Cache
+ * @method static remember(string $key, int $ttl, Callable $callback)
+ */
 class SecondaryCache extends Facade {
+    /**
+     * @return string
+     */
     protected static function getFacadeAccessor()
     {
         return CacheManager::class;
